bec456d5fddb8a44424db90a1f095e55f6ed07b2
JGalvisS <mejorquemada@hotmail.com>
2026-03-24 13:26:06 -0500

    varibles

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,21 @@
 print("hello world")
+
+#Variables
+y=20
+x=52
+print(x)
+
+# if basic
+if x > y:
+    print("y is greater than x")
+    
+#Casting
+#Use type to know what kind of varible it is 
+type_x = type(x) 
+print(type_x)
+#Use str to castint to sstring 
+str_y= str(y)
+type_y= type(str_y) 
+print(type_y)
+
+
