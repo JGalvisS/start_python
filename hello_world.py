d450ad24025ec2af1c35fe9c6cefe46577792bf0
JGalvisS <mejorquemada@hotmail.com>
2026-03-26 15:57:20 -0500

    collections

--- hello_world.py
+++ hello_world.py
@@ -13,9 +13,25 @@ if x > y:
 #Use type to know what kind of varible it is 
 type_x = type(x) 
 print(type_x)
-#Use str to castint to sstring 
+#Use str to casting to string or .format
 str_y= str(y)
 type_y= type(str_y) 
 print(type_y)
+new_string= "this is a number {}"
+print(new_string.format(x))
 
-
+#tipos de colecciociones
+#dict {}- clave/valor es mutable
+my_dirct={"name":"jose","age":26}
+print(my_dirct)
+#list [] mantiene el orden de insercion, es mutable con append(), pop()
+my_list=["mary","alvaro","jose"]
+print(my_list)
+my_list.append("carlos")
+print(my_list)
+#tupla () no se puede modificar 
+my_tupla= ("margarita", "rosa","girasol")
+print(my_tupla)
+#set no mantiene el orden, permite agregar y eliminar elemtos, Elimina los duplicados 
+my_set={"elsa","sebatian","mario"}
+print(my_set)
